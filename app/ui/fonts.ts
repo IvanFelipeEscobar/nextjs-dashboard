6135f138ac28c1de9e313c1bfbaf16983ba62284
Ivan Escobar <ivnescbr1989@gmail.com>
2023-11-08 18:42:31 -0500

    add optimized images

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
 import { Inter, Lusitana } from "next/font/google";
+//Next optimizes fonts when using next/font module. it downloads fonts and hosts them w/ other static files
 
 export const inter = Inter(
     { subsets: ["latin"] }
@@ -7,3 +8,4 @@ export const lusitana = Lusitana({
     weight: ['400', '700'], 
     subsets: ['latin']
 });
+
